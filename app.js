bc2557a56327f16b1db14ddd831708377bb9fa8b
edjunma <junwenma@buffalo.edu>
2019-07-29 23:21:37 -0400

    added conversion functions for celsius, fahrenheit, and kelvin

--- app.js
+++ app.js
@@ -2,9 +2,30 @@ const celsiusInput = document.querySelector('#celsius > input');
 const fahrenheitInput = document.querySelector('#fahrenheit > input');
 const kelvinInput = document.querySelector('#kelvin > input');
 
-celsiusInput.addEventListener('input', function() {
+function celsiusToFahrenheitAndKelvin() {
 	const cTemp = parseFloat(celsiusInput.value);
 	const fTemp = cTemp * (9 / 5) + 32;
 	const kTemp = cTemp + 273.15;
-	console.log(kTemp);
-});
+	fahrenheitInput.value = fTemp;
+	kelvinInput.value = kTemp;
+}
+
+function fahrenheitToCelsiusAndKelvin() {
+	const fTemp = parseFloat(fahrenheitInput.value);
+	const cTemp = (fTemp - 32) * (5 / 9);
+	const kTemp = ((fTemp + 459.67) * 5) / 9;
+	celsiusInput.value = cTemp;
+	kelvinInput.value = kTemp;
+}
+
+function kelvinToCelsiusAndFahrenheit() {
+	const kTemp = parseFloat(kelvinInput.value);
+	const cTemp = kTemp - 273.15;
+	const fTemp = (9 / 5) * (kTemp - 273) + 32;
+	celsiusInput.value = cTemp;
+	fahrenheitInput.value = fTemp;
+}
+
+celsiusInput.addEventListener('input', celsiusToFahrenheitAndKelvin);
+fahrenheitInput.addEventListener('input', fahrenheitToCelsiusAndKelvin);
+kelvinInput.addEventListener('input', kelvinToCelsiusAndFahrenheit);
